1daf259a1be524b1ade903aeb21feeae7268d494
Kendal Droddy <kdroddy001@gmail.com>
2019-02-03 21:43:36 -0600

    Added a few lines of formatting to match Rose's hpp file format

--- dataRead.hpp
+++ dataRead.hpp
@@ -1,13 +1,18 @@
-/******************************************************************
+/*******************************************************************************
 ** Program: Capstone Project - Team Homam - W2019
-** Author: Kendal Droddy (Rozalija Zibrat, Matthew Garner)
+** Author: Rozalija Zibrat (Kendal Droddy, Matthew Garner)
 ** Date: 17 March 2019
-** Description: A murder mystery game where the player has 
-** to interrogate suspects, investigate the crime scene, search 
-** for evidence, and ultimately decide who committed the murder.
-*******************************************************************/
+** Description: A murder mystery game where the player has to interrogate
+** suspects, investigate the crime scene, search for evidence, and ultimately
+** decide who committed the murder.
+*******************************************************************************/
+
 #ifndef DATAREAD_HPP
 #define DATAREAD_HPP
+
+/*------------------------------------------------------------------------------
+    LIBRARIES
+------------------------------------------------------------------------------*/
 
 #include <iostream>
 #include <fstream>
@@ -15,11 +20,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*********************************************************************
+/*******************************************************************************
 **                     readFileDefault
 ** Description: Uses getline to read in and display a file exactly
 ** as its typed in the text file
-*********************************************************************/
+*******************************************************************************/
 void readFileDefault(std::ifstream &fileName)
 {
   std::string input;
@@ -35,11 +40,11 @@ void readFileDefault(std::ifstream &fileName)
   fileName.seekg(0, std::ios::beg);
 }
 
-/*********************************************************************
+/*******************************************************************************
 **                     readFileChar
 ** Description: Uses getchar to read in and display a file by char
 ** instead of by line - allows more customization for displaying text
-*********************************************************************/
+*******************************************************************************/
 void readFileChar(std::ifstream &fileName)
 {
   char ch;
@@ -55,12 +60,12 @@ void readFileChar(std::ifstream &fileName)
   fileName.seekg(0, std::ios::beg);
 }
 
-/*********************************************************************
+/*******************************************************************************
 **                     calcWindowSize
 ** Description: Ensures terminal window is of sufficient size to
 ** fully enjoy the game. Prompts user to adjust terminal if size
 ** does not meet minimum requirements.
-*********************************************************************/
+*******************************************************************************/
 void calcWindowSize()
 {
   int cols = 80;
